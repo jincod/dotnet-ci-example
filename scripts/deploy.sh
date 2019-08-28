9c7a637de31a17b86912e3cc790b6373f14eab95
jincod <vadim.abdrashitov@gmail.com>
2019-08-28 09:47:12 +0500

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,6 @@ project_name=$(basename $project_file .csproj)
 package_version=$(cat $project_file | grep -oP '<PackageVersion>(.*)<\/PackageVersion>' | sed "s/<PackageVersion>\|<\/PackageVersion>//g")
 status_code=$(curl -s -o /dev/null -I -w "%{http_code}" https://$GITHUB_ACTOR:$GITHUB_TOKEN_2@nuget.pkg.github.com/$GITHUB_ACTOR/${project_name,,}/$package_version.json)
 
-echo $status_code
-
 if [ $status_code = 200 ]; then
     echo "skip..."
 else
@@ -19,6 +17,5 @@ else
     NUGET_CONFIG="${NUGET_CONFIG//GITHUB_TOKEN/$GITHUB_TOKEN_2}"
     NUGET_CONFIG="${NUGET_CONFIG//GITHUB_ACTOR/$GITHUB_ACTOR}"
     echo $NUGET_CONFIG > nuget.config
-    cat nuget.config
     dotnet nuget push *.nupkg -s https://nuget.pkg.github.com/$GITHUB_ACTOR/index.json -k GitHubPackageRegistry
 fi
