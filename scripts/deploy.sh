c8cf0fe98493b8ac30b995cb6a021743354de604
jincod <vadim.abdrashitov@gmail.com>
2019-08-28 10:00:26 +0500

    debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,9 @@ project_name=$(basename $project_file .csproj)
 package_version=$(cat $project_file | grep -oP '<PackageVersion>(.*)<\/PackageVersion>' | sed "s/<PackageVersion>\|<\/PackageVersion>//g")
 status_code=$(curl -s -o /dev/null -I -w "%{http_code}" https://$GITHUB_ACTOR:$GITHUB_TOKEN_2@nuget.pkg.github.com/$GITHUB_ACTOR/${project_name,,}/$package_version.json)
 
+echo $package_version
+echo $status_code
+
 if [ $status_code = 200 ]; then
     echo "skip..."
 else
