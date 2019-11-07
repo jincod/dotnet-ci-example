1e68b6d7adccb0c407226970e81181168e4569ea
jincod <vadim.abdrashitov@gmail.com>
2019-11-07 11:58:54 +0000

    revert

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ project_file=$1
 project_name=$(basename $project_file .csproj)
 
 package_version=$(cat $project_file | grep -oP '<PackageVersion>(.*)<\/PackageVersion>' | sed "s/<PackageVersion>\|<\/PackageVersion>//g")
-status_code=$(curl -s -o /dev/null -I -w "%{http_code}" https://$GITHUB_ACTOR:$GITHUB_TOKEN@nuget.pkg.github.com/$GITHUB_ACTOR/${project_name,,}/$package_version.json)
+status_code=$(curl -s -o /dev/null -I -w "%{http_code}" https://$GITHUB_ACTOR:$GITHUB_TOKEN_2@nuget.pkg.github.com/$GITHUB_ACTOR/${project_name,,}/$package_version.json)
 
 echo $package_version
 echo $status_code
@@ -17,7 +17,7 @@ else
     echo "publish..."
     dotnet pack $project_file --configuration Release --output "${PWD}"
     NUGET_CONFIG=`cat nuget.config`
-    NUGET_CONFIG="${NUGET_CONFIG//GITHUB_TOKEN/$GITHUB_TOKEN}"
+    NUGET_CONFIG="${NUGET_CONFIG//GITHUB_TOKEN/$GITHUB_TOKEN_2}"
     NUGET_CONFIG="${NUGET_CONFIG//GITHUB_ACTOR/$GITHUB_ACTOR}"
     echo $NUGET_CONFIG > nuget.config
     dotnet nuget push *.nupkg -s https://nuget.pkg.github.com/$GITHUB_ACTOR/index.json -k GitHubPackageRegistry
